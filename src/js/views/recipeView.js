43b0cc0c6d653dfc26d1187e2a66105ff41f39c8
Ahmed-walid <ahmed.ibrahim003@eng-st.cu.edu.eg>
2023-02-18 23:24:24 +0200

    fixing small conflict with Fractional package

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './view.js';
 import icons from '../../img/icons.svg'; // Parcel 2
-import { Fraction } from 'fractional';
+import fracty from "fracty";
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -31,7 +31,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
         </svg>
         <div class="recipe__quantity">${
-          ing.quantity ? new Fraction(Number(ing.quantity)).toString() : ''
+          ing.quantity ? fracty(Number(ing.quantity)).toString() : ''
         }</div>
         <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
--- src/js/views/view.js
+++ src/js/views/view.js
@@ -1,5 +1,5 @@
 import icons from '../../img/icons.svg'; // Parcel 2
-import { Fraction } from 'fractional';
+import fracty from "fracty";
 
 // common code to all views
 export default class View {
