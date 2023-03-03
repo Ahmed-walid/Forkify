d1b6308a0b09656f0c672193cfe67f28b227f87f
Ahmed-walid <ahmed.ibrahim003@eng-st.cu.edu.eg>
2023-03-03 22:36:40 +0200

    updating repo

--- src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RESULT_PAGE_SIZE = 10;
-export const KEY = '598f0c2e-824c-47d5-811b-72034f20707c';
-export const MODAL_CLOSE_SEC = 2.5;
